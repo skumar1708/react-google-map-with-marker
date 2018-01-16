e65f9330bafb8a1893d9ecdbb498d6f1f9e3f38e
Sharvan Sharma <ssh216@sapient.com>
2018-01-16 14:09:32 +0530

    changes new 2

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -10,12 +10,16 @@ export default class YourComponent extends Component {
   constructor(props){
     super(props);
     this.loadJS = this.loadJS.bind(this);
+    this.updateState = this.updateState.bind(this);
+    this.state = {
+      favourites:[]
+    }
   }
 
   componentDidMount() {
     // Connect the initMap() function within this class to the global window context,
     // so Google Maps can invoke it
-    window.initMap = this.initMap;
+    window.initMap = this.initMap.bind(this);
     // Asynchronously load the Google Maps script, passing in the callback reference
     this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A&callback=initMap')
 }
@@ -27,14 +31,16 @@ export default class YourComponent extends Component {
     script.async = true;
     ref.parentNode.insertBefore(script, ref);
   }
-
+  updateState(p){
+    console.log(p);
+  }
 initMap() {
     var map = new google.maps.Map( window.document.getElementById("map"),{
       zoom: 3,
       center: new google.maps.LatLng(39.414805,-94.1166931),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
+   
     var infowindow = new google.maps.InfoWindow(),marker, i;
 
     for (i = 0; i < data.users.length; i++) { 
@@ -43,13 +49,15 @@ initMap() {
           position: new google.maps.LatLng(data.users[i].Lat, data.users[i].Long),
           map: map
         });
-  
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        google.maps.event.addListener(marker, 'click', (function(marker, i, self) {
           return function() {
+            console.log("State is", self);
+            self.state.favourites.push(data.users[i].Name);
+            self.setState(self.state);
             infowindow.setContent(`<h1>${data.users[i].Name}</h1><br/><p>${data.users[i].Address}</p>`);
             infowindow.open(map, marker);
           }
-        })(marker, i));
+        })(marker, i,this));
       }
       
     }
@@ -58,6 +66,18 @@ initMap() {
     return (
       <div style={divStyle}>
         <h1> Put your solution here!</h1>
+        <div> 
+          <h3>My Favourite places</h3>
+
+          
+          {
+            this.state.favourites.filter(function(item, pos, self) {
+                   return self.indexOf(item) == pos;
+              }).map(function(val,x){
+                return <p key={x}>{val}</p>
+             })
+          }
+        </div>
         <div id="map" style={mapStyle}></div>
       </div>
     );
